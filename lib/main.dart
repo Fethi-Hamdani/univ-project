e1a01d713290d96a1de196be95615a3f4ff3e077
Splimter <soheybemir@hotmail.fr>
2019-04-10 23:01:13 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
+class Widget extends StatefulWidget {
+  createState() {
+    return WidgetState();
+  }
+}
 
+class WidgetState extends State<Widget> {
+  Widget build(context) {
+    return ;
+  }
+}
